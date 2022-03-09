418793e0670331204226eb79b64502251afbc9e5
mukilgs <61052517+mukilgs@users.noreply.github.com>
2022-03-09 15:26:12 +0530

    Create CustomerRepo.java

---
+++ CustomerRepo.java
@@ -0,0 +1,10 @@
+package com.example.registration;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepo extends JpaRepository<Customers, Integer>{
+	
+	
+
+}
