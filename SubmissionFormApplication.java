12a59ccbb9b32560af61051ef8a70152e1676bb4
mukilgs <61052517+mukilgs@users.noreply.github.com>
2022-03-09 15:27:20 +0530

    Create SubmissionFormApplication.java

---
+++ SubmissionFormApplication.java
@@ -0,0 +1,25 @@
+package com.example.registration;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@ComponentScan
+@SpringBootApplication
+public class SubmissionFormApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app)
+	{
+		return app.sources(SubmissionFormApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmissionFormApplication.class, args);
+	}
+
+}
